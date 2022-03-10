149a891c7a3c9847e1e6b9830482c01409f9e817
AlexRuedaPayen <alexruedapayen@gmail.com>
2022-03-10 13:21:21 +0100

    Stock class modified, stock_MA is now a class field

--- Alexandre/collect_data.py
+++ Alexandre/collect_data.py
@@ -4,13 +4,16 @@ import datetime
 
 class Stock:
     def __init__(self,name):
-        self.stock=pandas.read_csv(filepath_or_buffer='./Data/'+name+'.csv')
+        self.stock=pandas.read_csv(filepath_or_buffer='../Data/'+name+'.csv')
         self.stock.index=self.stock['Date']
         print(f'Data from '+ str(min(self.stock['Date'].values))+' to '+str(max(self.stock['Date'].values)))
     def MA(self,day=7):
         start_date=str(datetime.datetime.strptime(min(self.stock['Date'].values), "%Y-%m-%d")+ datetime.timedelta(days=day))
         end_date=max(self.stock['Date'].values)
-        return(((self.stock).rolling(window=day).mean()).loc[start_date:end_date, :])
+        self.stock_MA=(((self.stock).rolling(window=day).mean()).loc[start_date:end_date, :])
 
 RUI_PA=Stock(name='RUI.PA')
-print(RUI_PA.MA())
+RUI_PA.MA()
+
+print(RUI_PA.stock)
+print(RUI_PA.stock_MA)
