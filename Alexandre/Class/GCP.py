c7ec895dac3ec6d61cb3b51a6bc3d6403d39d706
AlexRuedaPayen <alexruedapayen@gmail.com>
2022-03-19 23:43:38 +0100

    ssh connection now possible

--- Alexandre/Class/GCP.py
+++ Alexandre/Class/GCP.py
@@ -1,26 +1,42 @@
+import paramiko,scp
+
+
 class GCP:
 
-    import paramiko
-
-    def __init__(self,host="MacAlexandre@34.125.182.253",
-                 keyfile="~/.ssh/VM-1-GCP-Instance1/key",
-                 class_=["Stock"],
-                 data_=["RUI.PA"]):
+    def __init__(self,
+                host="34.68.238.69",
+                user="MacAlexandre_GCP_VM1",
+                keyfile="/Users/alexandreprofessional2/Desktop/key/key_GCP_VM1",
+                class_=["Stock"],
+                data_=["RUI.PA"]):
         ssh = paramiko.SSHClient()
         self.ssh=ssh 
-        k = paramiko.RSAKey.from_private_key_file(keyfile)
+        self.host=host
+        self.user=user
+        from getpass import getpass
+        password = getpass()
+        k = paramiko.RSAKey.from_private_key_file(keyfile,password=password)
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(hostname=host, username="AlexRuedaPayen", pkey=k)
+        ssh.connect(hostname=host, username=user, pkey=k)
         self.class_=class_
+        """
+        stdin,stdout,stderr=ssh.exec_command('pwd')
+        print(stdout.read().decode('utf8'))
+        #print(stdin.read().decode('utf-8'),stdout.read().decode('utf-8'))
+        stdin.close()
+        stdout.close()
+        stderr.close()"""
 
     def __enter__(self):
-        file_path_origin="./Alexandre/class/"
-        file_path_destination="~/Projects/Financial_Indices/Alexandre/class/"
+        file_path_origin="./Alexandre/Class/"
+        file_path_destination="~/Projects/Financial_Indices/Alexandre/Class/"
+        self.scp=scp.SCPClient(self.ssh.get_transport())
         for i in self.class_:
-            file_path_class_origin=file_path_origin+i
-            file_path_class_destination=file_path_destination+i
-            subprocess.run(["scp", file_path_class_origin, "USER@SERVER:"+file_path_class_destination])
-        return()
+            file_path_class_origin=file_path_origin+i+".py"
+            file_path_class_destination=self.user+"@instance-2:"+file_path_destination+i+".py"
+            self.scp.put(file_path_class_origin,file_path_class_destination)
+            print("File "+file_path_class_destination+" created")
+        return(self)
 
     def run(self,script):
         file_path_script_origin="./Alexandre/script/"+script
@@ -33,3 +49,6 @@ class GCP:
         for i in file:
             subprocess.run(["scp", "USER@SERVER:"+i,i])
         self.ssh.disconnect()
+
+a=GCP()
+a.__enter__()
