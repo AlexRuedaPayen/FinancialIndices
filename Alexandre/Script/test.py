a073bf3fd51080798a241d40bddee7c78b482b10
AlexRuedaPayen <alexruedapayen@gmail.com>
2022-03-20 13:14:32 +0100

    commit 20_03_21

--- Alexandre/Script/test.py
+++ Alexandre/Script/test.py
@@ -6,12 +6,14 @@ import unittest,pandas
 
 class TestGCP(unittest.TestCase):
     def fun_A(self):
-        with GCP as f:
-            f.run("fun_A")
-            fun_A_with_GCP=pandas.read_csv("./Alexandre/Data_GCP/fun_A_res.csv")
-            from Alexandre.Script.fun_A import fun_A
-            fun_A_without_GCP=fun_A()
-            self.assertEqual(fun_A_with_GCP,fun_A_without_GCP)
+        fun_A_with_GCP=pandas.read_csv("./Alexandre/Data_GCP/fun_A_res.csv")
+        from Alexandre.Script.fun_A import fun_A
+        fun_A_without_GCP=fun_A()
+        self.assertEqual(fun_A_with_GCP,fun_A_without_GCP)
 
 if __name__ == '__main__':
+
+    with GCP() as f:
+        f.run("fun_A")
+
     unittest.main()
